2819cdad53a87bab3b0dbf32dd17fda4c17e24cb
Cielle Carlton <cielle.carlton13@gmail.com>
2021-01-05 20:40:09 -0500

    editing connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,3 @@
-// var mysql = require("mysql");
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "cielle13",
-//   database: "burgers_db"
-// });
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-// module.exports = connection;
-
 var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL) {
@@ -24,7 +7,7 @@ connection = mysql.createConnection(process.env.JAWSDB_URL);
   host: “localhost”,
   port: 3306,
   user: “root”,
-  password: “Cielle13“,
+  password: “cielle13“,
   database: “burgers_db”
 });
 };
